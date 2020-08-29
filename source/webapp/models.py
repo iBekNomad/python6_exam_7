6d197aa3cd44afbbccd930bc2ea1c5ed6472fcdf
Aibek <nomad.aibek@gmail.com>
2020-08-29 11:46:44 +0600

    Добавлены модели Poll и Choice. Сделана миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=2500, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return f'{self.question} - {self.created_at}'
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='choices', verbose_name='Опрос')
+
+    def __str__(self):
+        return f'{self.text}'
